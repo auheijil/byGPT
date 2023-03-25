13ee0b9256eb841c01db67031079140de365ec0f
auheijil <1079547493@qq.com>
2023-03-25 15:43:49 +0800

    initial 7

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-const submitBtn = document.getElementById('submit-btn');
-const questionInput = document.getElementById('question-input');
-const outputContainer = document.querySelector('.output-container');
-//const apikeyInput = document.getElementById('apikey-input');
+const apiKeyInput = document.getElementById('api-key-input');
+const apiKeySubmit = document.getElementById('api-key-submit');
+const inputText = document.getElementById('input-text');
+const submitButton = document.getElementById('submit-button');
+const outputText = document.getElementById('output-text');
 
-submitBtn.addEventListener('click', function() {
-  const question = questionInput.value;
-  const apikey = "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"
-  if (question && apikey) {
-    fetch('https://api.openai.com/v1/engine/gpt-3.5-turbo/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apikey}`
-      },
-      body: JSON.stringify({
-        prompt: question + '\n',
-        max_tokens: 50,
-        n: 1,
-        stop: '\n'
-      })
-    })
+let apiKey = '';
+
+apiKeySubmit.addEventListener('click', () => {
+  apiKey = apiKeyInput.value;
+  apiKeyInput.value = '';
+  alert('API Key submitted!');
+});
+
+submitButton.addEventListener('click', () => {
+  const question = inputText.value;
+  inputText.value = '';
+  fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      prompt: question,
+      max_tokens: 100,
+      n: 1,
+      stop: ['\n'],
+    }),
+  })
     .then(response => response.json())
     .then(data => {
       const answer = data.choices[0].text.trim();
-      outputContainer.innerHTML = `<p>${answer}</p>`;
+      outputText.textContent = answer;
     })
     .catch(error => {
-      outputContainer.innerHTML = `<p>API调用失败，请检查您的API Key是否正确。</p>`;
+      console.error(error);
+      outputText.textContent = 'Error: ' + error.message;
     });
-  } else {
-    outputContainer.innerHTML = `<p>请输入您的问题和API Key。</p>`;
-  }
 });
